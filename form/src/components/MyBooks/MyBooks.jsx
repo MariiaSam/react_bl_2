f6ce4d26a91be1f4eb3b634a0342d9d2da897413
MariiaSam <kontragent@ukr.net>
2024-03-13 13:14:25 +0200

    add

--- form/src/components/MyBooks/MyBooks.jsx
+++ form/src/components/MyBooks/MyBooks.jsx
@@ -17,19 +17,46 @@ class MyBooks extends Component {
     ],
   };
 
+  isDublicate({ title, author }) {
+    const { books } = this.state;
+    const normalizedTitle = title.toLowerCase();
+    const normalizedAuthor = author.toLowerCase();
+
+    const dublicate = books.find((item) => {
+      const normalizedCurrentTitle = item.title.toLowerCase();
+      const normalizedCurrentAuthor = item.author.toLowerCase();
+      return (
+        normalizedCurrentTitle === normalizedTitle &&
+        normalizedCurrentAuthor === normalizedAuthor
+      );
+    });
+
+    return Boolean(dublicate);
+  }
+
   addBook = (data) => {
-    this.setState (( {books}) => {
-     const newBook = {
+    if (this.isDublicate(data)) {
+      return alert(
+        `Book with ${data.title} and ${data.author} already in list`
+      );
+    }
+
+    this.setState(({ books }) => {
+      const newBook = {
         id: nanoid(),
         ...data,
-    }
-    return {book: [...books, newBook]} // ... book - старі книги, до яких додаю нову 
-    })
-}; //books - prevState
+      };
+
+      return {
+        books: [...books, newBook],
+      };
+    });
+    // ... book - старі книги, до яких додаю нову
+  }; //books - prevState
 
   render() {
     const { books } = this.state;
-    const { addBook } = this.setState;
+    const { addBook } = this;
 
     return (
       <div className={styles.wrapper}>
