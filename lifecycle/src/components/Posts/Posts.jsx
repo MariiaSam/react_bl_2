81e2200531bfcde11cb8123cb439a58d19264457
MariiaSam <kontragent@ukr.net>
2024-03-15 17:58:46 +0200

    add

--- lifecycle/src/components/Posts/Posts.jsx
+++ lifecycle/src/components/Posts/Posts.jsx
@@ -1,29 +1,47 @@
 import { Component } from "react";
+import axios from "axios";
 
-
-import styles from './posts.module.css'
+import styles from "./posts.module.css";
 
 class Posts extends Component {
-    state = {
-        posts: []
-    }
+  state = {
+    posts: [],
+    loading: false,
+  };
 
-    render() {
+  componentDidMount() {
+    this.setState({
+      loading: true,
+    });
+    axios.get("https://jsonplaceholder.typicode.com/posts").then(({ data }) => {
+      if (data?.length) {
+        this.setState({
+          loading: false,
+          posts: data?.length ? data : [],
+        });
+      }
+    });
+  }
 
-        const {posts} = this.state
-        const elements = posts.map(({ id, title, body }) => (<li key={id} className={styles.item}>
-            <h3>{title}</h3>
-            <p>{body}</p>
-        </li>));
+  render() {
+    // const postsRequest = axios('https://jsonplaceholder.typicode.com/posts') поверне проміс, а не розмітку
+    const { posts, loading } = this.state;
+    const elements = posts.map(({ id, title, body }) => (
+      <li key={id} className={styles.item}>
+        <h3>{title}</h3>
+        <p>{body}</p>
+      </li>
+    ));
 
-        return(
-            <ul className={styles.list}>
-                {elements}
-            </ul>
-            )
-    
-    }
-
+    return (
+      <>
+        {loading && <p>...Loading</p>}
+        {Boolean(elements.length) && (
+          <ul className={styles.list}>{elements}</ul>
+        )}
+      </>
+    );
+  }
 }
 
-export default Posts
+export default Posts;
