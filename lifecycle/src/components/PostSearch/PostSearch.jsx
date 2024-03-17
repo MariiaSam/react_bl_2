c90b1940a9a9cabce65873c9662c435257840058
MariiaSam <kontragent@ukr.net>
2024-03-17 20:00:53 +0200

    add

--- lifecycle/src/components/PostSearch/PostSearch.jsx
+++ lifecycle/src/components/PostSearch/PostSearch.jsx
@@ -16,12 +16,7 @@ class PostsSearch extends Component {
 
   render() {
     const { posts, loading, error } = this.state;
-    const elements = posts.map(({ id, title, body }) => (
-      <li key={id} className={styles.item}>
-        <h3>{title}</h3>
-        <p>{body}</p>
-      </li>
-    ));
+  
 
     return (
       <>
